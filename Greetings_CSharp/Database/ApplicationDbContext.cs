7469b1c104f6ca6b83b16c9d4828a366786febac
mxaba <samuel.mcebo@gmail.com>
2021-08-27 18:01:37 +0200

    fixed the database

--- Greetings_CSharp/Database/ApplicationDbContext.cs
+++ Greetings_CSharp/Database/ApplicationDbContext.cs
@@ -7,6 +7,12 @@ namespace Greetings_CSharp.Database
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // modelBuilder.UsePostgresConventions();
+        }
 
         public DbSet<Greetings> Greetings { get; set; }
     }
