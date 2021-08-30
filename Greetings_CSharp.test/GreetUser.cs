2678f2e577c51a3151325d7a65e48d0d85edfa7d
mxaba <samuel.mcebo@gmail.com>
2021-08-30 12:50:04 +0200

    Test

--- Greetings_CSharp.test/GreetUser.cs
+++ Greetings_CSharp.test/GreetUser.cs
@@ -10,35 +10,29 @@ namespace Greetings_CSharp.test
     public class GreetUser
     {
         private ApplicationDbContext testDbContext;
-        private CreateReadUpdateDelete crud;
+        private ICreateReadUpdateDelete createReadUpdateDelete;
         private GreetMessage message;
 
         public GreetUser(DatabaseFixture databaseFixture)
         {
             testDbContext = databaseFixture.DbContext;
-        }
-
-        private void CrudConstructor(){
-            crud = new CreateReadUpdateDelete(testDbContext);
             message = new GreetMessage();
         }
 
         [Fact]
         public void GreetMceboInAllLanguage()
         {
-            CrudConstructor();
             var greetBind = new Greetings { Name="Mcebo", English=0, Spanish=0,  Isizulu=0, Counts=0};
             var grettingsDatabase = testDbContext.Greetings;
             Assert.Equal("Sawubona, Mcebo", message.Message(greetBind, "isizulu"));
             Assert.Equal("Hello, Mcebo", message.Message(greetBind, "english"));
             Assert.Equal("Hola, Mcebo", message.Message(greetBind, "spanish"));
-            // Assert.Equal();
+            // Assert.Equal(1, createReadUpdateDelete.CountData());
         }
 
         [Fact]
         public void GreetAndreInAllLanguage()
         {
-            CrudConstructor();
             var greetBind = new Greetings { Name="Andre", English=0, Spanish=0,  Isizulu=0, Counts=0};
             var grettingsDatabase = testDbContext.Greetings;
             Assert.Equal("Sawubona, Andre", message.Message(greetBind, "isizulu"));
