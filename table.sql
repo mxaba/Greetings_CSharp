1a9c4f9b7873999a0059bdfc5566da36894a401f
mxaba <samuel.mcebo@gmail.com>
2021-09-01 15:38:10 +0200

    added a database creation and sql

--- table.sql
+++ table.sql
@@ -1,2 +1,8 @@
 create table Greetings (
+    id serial not null,
+    names text not null,
+    english int not null,
+    spanish int not null,
+    isizulu int not null,
+    counts int not null
 );
