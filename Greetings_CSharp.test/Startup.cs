0e66a677e1920ff20d33f58ce630a4f59d6376e4
mxaba <samuel.mcebo@gmail.com>
2021-09-01 15:37:19 +0200

    added a database creation

--- Greetings_CSharp.test/Startup.cs
+++ Greetings_CSharp.test/Startup.cs
@@ -13,7 +13,7 @@ namespace Greetings_CSharp.test
         {
             services.AddSingleton<ICreateReadUpdateDelete, CreateReadUpdateDelete>();
 
-            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql("Host=ec2-18-214-238-28.compute-1.amazonaws.com;Database=d5etmdfu8k1n23;Port=5432;User Id=vztsnbyxsnkopv;Password=fb36d0e1d5ac82b96468cb9c2e60f47fba10efb2ed3ffda4ea23e7b85794adb2;SSL Mode=Require;Trust Server Certificate=True;"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql("Server=localhost;Database=greetings;Port=5432;User Id=mxaba;Password=MyPasswordMCB;"));
         }
     }
 }
